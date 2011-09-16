fb69d30f5dde399786beefa75ae056352efb9c27
Gaurav Saini <gaurav.saini@qualtech-consultants.com>
2011-09-16 20:09:03 +0530

    interval changed

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ var channel = new function () {
   // clear old callbacks, they can hang around for at most 30 seconds.
   setInterval(function () {
     var now = new Date();
-    while (callbacks.length > 0 && now - callbacks[0].timestamp > 30*1000) {
+    while (callbacks.length > 0 && now - callbacks[0].timestamp > 20*1000) {
       callbacks.shift().callback([]);
     }
   }, 3000);
